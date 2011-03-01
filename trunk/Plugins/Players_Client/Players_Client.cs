05cf2dc838a2efcd919e83dd67f443273ab43d3b
codekrash <codekrash@gmail.com>
2011-03-01 21:12:00 +0000

    now have proper movement

--- trunk/Plugins/Players_Client/Players_Client.cs
+++ trunk/Plugins/Players_Client/Players_Client.cs
@@ -106,8 +106,9 @@ namespace ExtraMegaBlob
                 nodes["drone"].SetFixedYawAxis(true);
                 //OgreWindow.Instance.cameraNode.LookAt(
 
-                OgreWindow.g_m.MouseMoved += new MouseListener.MouseMovedHandler(g_m_MouseMoved);
-
+                OgreWindow.g_m.MouseMoved += new MouseListener.MouseMovedHandler(mouseMoved);
+                middlemousetimer.reset();
+                middlemousetimer.start();
                 ready = true;
             }
             catch (Exception ex)
@@ -166,14 +167,12 @@ namespace ExtraMegaBlob
             switch (ev._Keyword)
             {
                 case KeyWord.MOVEPLAYER:
-                    SceneNode node = nodes["drone"];
-                    ExtraMegaBlob.References.Vector3 loc = ExtraMegaBlob.References.Vector3.FromString(ev._Memories["loc"].Value);
-                    node.Translate(loc.toMogre);
-                    updateCam();
+
                     //OgreWindow.Instance.cameraNode.Position = m_CamPos * node._getDerivedPosition();
 
                     break;
                 case KeyWord.ROTATEPLAYER:
+                    //updateCam();
                     //ExtraMegaBlob.References.Vector3 loc = ExtraMegaBlob.References.Vector3.FromString(ev._Memories["loc"].Value);
                     //nodes["drone"].Translate(loc.toMogre);
                     break;
@@ -216,20 +215,30 @@ namespace ExtraMegaBlob
             return m_CamPos;
         }
         private float RotateScale_Camera = .001f;//mouse sensitivity
-        private bool g_m_MouseMoved(MouseEvent arg)
+        private bool mouseMoved(MouseEvent arg)
         {
-            SceneNode node = nodes["drone"];
-            SceneNode camnode = nodes["orbit0"];
             MouseState_NativePtr s = arg.state;
             if (arg.state.buttons == 2)
             {
-                node.Yaw(-s.X.rel * RotateScale_Camera);
-                
+                nodes["drone"].Yaw(-s.X.rel * RotateScale_Camera);
+                nodes["orbit0"].Pitch(s.Y.rel * RotateScale_Camera);
                 updateCam();
-                camnode.Pitch(s.Y.rel * RotateScale_Camera);
             }
 
-
+            float mouseZ = (float)OgreWindow.g_m.MouseState.Z.rel * .1f;
+            //chat(mouseZ.ToString());
+            if (mouseZ > 0)
+            {
+                middleMouseState = middleMouseStates.scrollup;
+                middlemousetimer.reset();
+                middlemousetimer.start();
+            }
+            else if (mouseZ < 0)
+            {
+                middleMouseState = middleMouseStates.scrolldown;
+                middlemousetimer.reset();
+                middlemousetimer.start();
+            }
 
             return true;
         }
@@ -237,10 +246,10 @@ namespace ExtraMegaBlob
         {
             SceneNode camnode = nodes["orbit"];
             SceneNode node = nodes["drone"];
-            Mogre.Vector3 focalPoint = new Mogre.Vector3(0f,0f,0f);
+            Mogre.Vector3 focalPoint = new Mogre.Vector3(0f, 0f, 0f);
             Quaternion direction = node.Orientation;
             direction.Normalise();
-            
+
             //Mogre.Vector3 m_CamPos = getOrbitalPosition(focalPoint, direction);
             //OgreWindow.Instance.cameraNode.LookAt(focalPoint, Node.TransformSpace.TS_PARENT);
             //OgreWindow.Instance.cameraNode.Position = m_CamPos + focalPoint;
@@ -251,6 +260,28 @@ namespace ExtraMegaBlob
             camnode.LookAt(node.Position, Node.TransformSpace.TS_WORLD);
 
         }
+        private timer middlemousetimer = new timer(mmbClutch);
+        private enum middleMouseStates
+        {
+            scrollup,
+            scrolldown,
+            idle
+        }
+        private middleMouseStates middleMouseState = middleMouseStates.idle;
+        private float MoveScale_Camera_forwardback = 0f;
+        private float MoveScale_Camera_leftright = 0f;
+        private float MoveScale_Camera_updown = 0f;
+        private Mogre.Vector3 TranslateVector_Camera = new Mogre.Vector3();
+        private const float speedcap_forwardback = .15f;
+        private const float speedcap_leftright = .15f;
+        private const float speedcap_updown = .5f;
+        private const float incr_forwardback = .0005f;
+        private const float incr_leftright = .0005f;
+        private const float incr_updown = .0001f;
+        private const float brakes_updown = incr_updown * 2;
+        private const float brakes_forwardback = incr_forwardback * 2;
+        private const float brakes_leftright = incr_leftright * 2;
+        private static TimeSpan mmbClutch = new TimeSpan(0, 0, 0, 0, 100);
         public override void updateHook()
         {
             if (t.elapsed)
@@ -261,6 +292,102 @@ namespace ExtraMegaBlob
             if (ready)
             {
                 walkState.AddTime(.01f);
+
+                try
+                {
+
+
+                    if (middlemousetimer.elapsed)
+                    {
+                        middleMouseState = middleMouseStates.idle;
+                    }
+
+
+
+                    if (OgreWindow.g_kb.IsKeyDown(MOIS.KeyCode.KC_W))
+                    {
+                        if (MoveScale_Camera_forwardback > -speedcap_forwardback)
+                            MoveScale_Camera_forwardback -= incr_forwardback;
+                    }
+                    else if (OgreWindow.g_kb.IsKeyDown(MOIS.KeyCode.KC_S))
+                    {
+                        if (MoveScale_Camera_forwardback < speedcap_forwardback)
+                            MoveScale_Camera_forwardback += incr_forwardback;
+                    }
+                    else if (MoveScale_Camera_forwardback != 0f)
+                    {
+                        if (MoveScale_Camera_forwardback > 0f)
+                            MoveScale_Camera_forwardback -= incr_forwardback;
+                        else
+                            MoveScale_Camera_forwardback += incr_forwardback;
+                        if (MoveScale_Camera_forwardback < brakes_forwardback && MoveScale_Camera_forwardback > -brakes_forwardback)
+                            MoveScale_Camera_forwardback = 0f;
+                    }
+                    else
+                    {
+                        MoveScale_Camera_forwardback = 0f;
+                    }
+                    if (OgreWindow.g_kb.IsKeyDown(MOIS.KeyCode.KC_A))
+                    {
+                        if (MoveScale_Camera_leftright > -speedcap_leftright)
+                            MoveScale_Camera_leftright -= incr_leftright;
+                    }
+                    else if (OgreWindow.g_kb.IsKeyDown(MOIS.KeyCode.KC_D))
+                    {
+                        if (MoveScale_Camera_leftright < speedcap_leftright)
+                            MoveScale_Camera_leftright += incr_leftright;
+                    }
+                    else if (MoveScale_Camera_leftright != 0f)
+                    {
+                        if (MoveScale_Camera_leftright > 0f)
+                            MoveScale_Camera_leftright -= incr_leftright;
+                        else
+                            MoveScale_Camera_leftright += incr_leftright;
+                        if (MoveScale_Camera_leftright < brakes_leftright && MoveScale_Camera_leftright > -brakes_leftright)
+                            MoveScale_Camera_leftright = 0f;
+                    }
+                    else
+                    {
+                        MoveScale_Camera_leftright = 0f;
+                    }
+
+                    if (middleMouseState == middleMouseStates.scrolldown)
+                    {
+                        if (MoveScale_Camera_updown > -speedcap_updown)
+                            MoveScale_Camera_updown -= incr_updown;
+                    }
+                    else if (middleMouseState == middleMouseStates.scrollup)
+                    {
+                        if (MoveScale_Camera_updown < speedcap_updown)
+                            MoveScale_Camera_updown += incr_updown;
+                    }
+                    else if (MoveScale_Camera_updown != 0f)
+                    {
+                        if (MoveScale_Camera_updown > 0f)
+                            MoveScale_Camera_updown -= incr_updown;
+                        else
+                            MoveScale_Camera_updown += incr_updown;
+                        if (MoveScale_Camera_updown < brakes_updown && MoveScale_Camera_updown > -brakes_updown)
+                            MoveScale_Camera_updown = 0f;
+                    }
+                    else
+                    {
+                        MoveScale_Camera_updown = 0f;
+                    }
+                }
+                catch { OgreWindow.Instance.log("couldn't wire up camera input"); }
+                #region gui updates
+                Mogre.Vector3 pos = OgreWindow.Instance.mCamera.Position;
+
+
+                OgreWindow.Instance.updateCoords(OgreWindow.UI_ELEMENT.label1, "X: " + pos.x.ToString("N"));
+                OgreWindow.Instance.updateCoords(OgreWindow.UI_ELEMENT.label2, "Y: " + pos.y.ToString("N"));
+                OgreWindow.Instance.updateCoords(OgreWindow.UI_ELEMENT.label3, "Z: " + pos.z.ToString("N"));
+                OgreWindow.Instance.updateCoords(OgreWindow.UI_ELEMENT.label4, "S1: " + MoveScale_Camera_forwardback.ToString("N") + " S2: " + MoveScale_Camera_leftright.ToString("N"));
+
+                OgreWindow.Instance.updateCoords(OgreWindow.UI_ELEMENT.textBox1, string.Format("{0}f, {1}f, {2}f", pos.x, pos.y, pos.z));
+
+                #endregion
             }
         }
         timer scaleLimiter = new timer(new TimeSpan(0, 0, 1));
@@ -268,7 +395,35 @@ namespace ExtraMegaBlob
         private bool ready = false;
         public override void frameHook(float interpolation)
         {
+            TranslateVector_Camera.z -= MoveScale_Camera_forwardback * (interpolation + 1);
+            TranslateVector_Camera.x -= MoveScale_Camera_leftright * (interpolation + 1);
+            TranslateVector_Camera.y += MoveScale_Camera_updown * (interpolation + 1);
+            //if (MoveScale_Camera_updown != 0)
+            //{
+            //    float s = MoveScale_Camera_updown * (interpolation + 1);
+            //    TranslateVector_Camera.y += s;
+            //    MoveScale_Camera_updown -= s;
+            //}
+            try
+            {
+                //Mogre.Vector3 translateTo = OgreWindow.Instance.cameraYawNode.Orientation * OgreWindow.Instance.cameraPitchNode.Orientation * TranslateVector_Camera;
+                if (TranslateVector_Camera.x != 0f || TranslateVector_Camera.y != 0f || TranslateVector_Camera.z != 0f)
+                {
+                    //OgreWindow.Instance.cameraNode.Translate(translateTo);
+                    //movePlayer(TranslateVector_Camera);
+                    //OgreWindow.Instance.cameraNode.Orientation.
+                    //nodes["drone"].Translate(nodes["orbit"]._getDerivedOrientation() * TranslateVector_Camera); //strafe works
+                    nodes["drone"].Translate(nodes["drone"]._getDerivedOrientation() * TranslateVector_Camera); //limited to x/z
+                    //nodes["drone"].Translate(nodes["orbit0"]._getDerivedOrientation() * TranslateVector_Camera); //not limited but a bit wierd
+                    updateCam();
 
+
+                }
+                TranslateVector_Camera = new Mogre.Vector3();
+            }
+            catch
+            {
+            }
         }
         private Random ran = new Random((int)DateTime.Now.Ticks);
 
